3188d52cd63e43347184603d29c72eb275f22ba0
Caio Macedo <caiomacedo123@outlook.com>
2025-03-31 15:26:26 -0300

    exmain included

---
+++ exmain.c
@@ -0,0 +1,29 @@
+/*
+
+* 0 - Lista de XXXX - Algoritmos e Estruturas de Dados III 
+* Autores: | > Caio Macedo Lima da Cruz.....RA: 2651378
+*          | > José Carlos Seben............RA: 2651130
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//TODO evite usar muito autocomplete do copilot, isso pode gerar muitos erros e confusões no código.
+//TODO comentar as linhas do código e explicar seu funcionamento abaixo.
+// 0. Template de código 
+int func() {
+    //TODO implemente a função aqui.
+}
+
+/* 
+? (O que ela faz?)
+? (Tratamentos)
+? (Lógica e resultados)
+!       input:  | output:
+!       input:  | output:
+*/
+
+int main(){
+    //TODO implemente o main e testes aqui.
+}

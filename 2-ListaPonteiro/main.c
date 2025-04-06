31180fa3d29abfec302abdc5859e10b91fba088d
Jos.c_s <josecarlospc001@gmail.com>
2025-04-05 21:02:46 -0300

    2 e 3 resolvidas tbm <3

--- 2-ListaPonteiro/main.c
+++ 2-ListaPonteiro/main.c
@@ -23,6 +23,7 @@ int func() {
 */
 
 /*
+Exercicio 1:
 1) Sendo p um ponteiro, qual a diferença entre: 
 •  p++;        
 •  (*p)++;         
@@ -31,7 +32,7 @@ int func() {
 */
 
 /*
-Resposta:
+Resposta exercicio 1:
     p++: Incrementa o ponteiro fazendo o apontar para a proxima "posição" sem estar incrementado 
     o valor do ponteiro e sim apenas sua posição. Comumente utilizado em Arrays onde o ponteiro vai estar 
     apontando para a posição 0 utilizando p++ o ponteiro vai apontar para a proxima posiçãodo
@@ -64,7 +65,30 @@ Resposta:
     a maquina estar acessando a posição 10 de um Array, vetor etc...
 */
 
-
 int main(){
-    
+     
+    //exercicio 2:
+    printf("Exercicio 2:\n ");
+    int y, *p, x;  //Declaração da variaveis sendo p um ponteiro.
+    y = 0;  //inicia y com valor de 0
+    p = &y;  //inicia p com o endereço de y
+    x = *p;  /* inicia x com o valor de y pois com o *p esta apontando para o valor
+                diferente de p que estava pontando para o local na memoria de y e nao o valor do seguinte */
+    x = 4;  //adiciona 4 como valor de x 
+    (*p)++;  //como na explicação da atividade 1 a posição vai ser mantida e vai ter uma incrementação no valor de y que passara a ser 1
+    x--;  //logica contraria de ++ utilizada para incrementar utiliza -- para diminuir o valor de x
+    (*p) += x;  /* faz a soma do valor do ponteiro de y que estava como 1 depois da incrementação com o valor de x
+                    que estava como 1 depois da decrementação somando como 4*/
+    printf ("y = %d\n", y);  //printa o valor de y apos todas as operações
+
+    //exercicio 3:
+    printf("\nExercicio 3:\n ");
+    int x2, *p2, **q2; 
+    p2 = &x2; 
+    q2 = &p2; 
+    x2 = 10; 
+    printf("%d\n", **q2); /*para resolver o problema apenas troque o &q2 que armazena a posição de memoria de p2
+                            por **q2 que por ser um ponteiro duplo armazena o valor de p2 que armazena o valor de x2 */
+
+    return(0); 
 }

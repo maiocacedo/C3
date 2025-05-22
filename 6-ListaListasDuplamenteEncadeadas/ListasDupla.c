50618bb1dc58260ce4a4fd56f1669ecec9405200
Caio Macedo <caiomacedo123@outlook.com>
2025-05-22 15:58:19 -0300

    Exericio 4

--- 6-ListaListasDuplamenteEncadeadas/ListasDupla.c
+++ 6-ListaListasDuplamenteEncadeadas/ListasDupla.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ListasDupla.h"
+#define ERR 0
+#define OK 1
+
+// Fila
+struct fila{
+    int info;
+    struct fila *prox;
+    struct fila *ant;
+}; typedef struct fila Fila;
+
+// No Descritor
+struct nodesc{
+    struct fila *ini;
+    struct fila *fim;
+}; 
 
 //Defini��o do tipo lista
 struct elemento{
@@ -9,6 +24,8 @@ struct elemento{
     struct elemento *prox;
 };
 typedef struct elemento Elem;
+
+
 
 Lista* cria_lista(){
     Lista* li = (Lista*) malloc(sizeof(Lista));
@@ -180,7 +197,7 @@ int insere_lista_ordenada(Lista *li, struct aluno al)
 {
     if (li == NULL) return 0;
 
-    Elem *no = (Elem *)malloc(sizeof(Elem));
+    Elem *no = malloc(sizeof *no);
     if (no == NULL) return 0;
 
     no->dados = al;
@@ -216,3 +233,143 @@ int insere_lista_ordenada(Lista *li, struct aluno al)
         return 1;
 }
 
+// EXERCICIO 4:
+
+// Função para inserção de elemento no inicio da lista circular
+int insere_lista_circular_inicio(Lista* li, struct aluno al){
+    if(li == NULL) return ERR;
+
+    Elem *no = malloc(sizeof *no);
+    if (no == NULL) return ERR;
+
+    no->dados = al;
+    
+    if(*li == NULL){
+        
+        no->prox = no;
+        no->ant = no;
+        *li = no;
+        
+        return OK;
+    } else{
+        
+        no->prox = *li;
+        no->ant = (*li)->ant;
+        (*li)->ant->prox = no;
+        (*li)->ant = no;
+        *li = no;
+        
+        return OK;
+    }
+
+}
+
+// Função para inserção de elemento no final de lista circular
+int insere_lista_circular_final(Lista* li, struct aluno al){
+    if(li == NULL) return ERR;
+
+    Elem *no = malloc(sizeof *no);
+    if (no == NULL) return ERR;
+    
+    no->dados = al;
+
+    if((*li) == NULL){
+        no->prox = no;
+        no->ant = no;
+        *li = no;
+        return OK;
+    } else{
+        no->prox = *li;
+        no->ant = (*li)->ant;
+        (*li)->ant->prox = no;
+        (*li)->ant = no;
+        return OK;
+    }
+}
+
+// Função para remvoer primeiro elemento de lista circular
+int remove_lista_circular_inicio(Lista* li){
+    if(li == NULL) return ERR;
+    if(*li == NULL) return ERR;
+
+    Elem *no = *li;
+
+    if(no->prox == no){
+        free(no);
+        *li = NULL;
+        return OK;
+    } else{
+        no->ant->prox = no->prox; // prox do ultimo aponta para o novo primeiro
+        no->prox->ant = no->ant; // ant do prox aponta para o novo ultimo
+        *li = no->prox; // novo primeiro
+        free(no); // libera o antigo primeiro
+        return OK;
+    }
+}
+
+// Função para remover ultimo elemento de lista circular
+int remove_lista_circular_final(Lista* li){
+    if(li == NULL) return ERR;
+    if(*li == NULL) return ERR;
+
+    Elem *no = (*li)->ant;
+    if(no->prox == no){
+        free(no);
+        *li = NULL;
+        return OK;
+    } else{
+        no->prox->ant = no->ant; 
+        no->ant->prox = no->prox; 
+        (*li)->ant = no->ant; // novo ultimo
+        free(no); // libera o antigo primeiro
+
+        return OK;
+    }
+}
+
+// Função para percorrer lista circular
+int percorre_lista_circular(Lista* li){
+    if(li == NULL) return ERR;
+    if(*li == NULL) return ERR;
+
+    Elem *no = *li;
+    do{
+        
+        printf("Matricula: %d\n",no->dados.matricula);
+        printf("Nome: %s\n",no->dados.nome);
+        printf("Notas: %f %f %f\n",no->dados.n1,
+                                   no->dados.n2,
+                                   no->dados.n3);
+        printf("-------------------------------\n");
+
+        no = no->prox;
+    }while(no != *li);
+    
+    return OK;
+}
+
+// EXERCICIO 5:
+
+// Função para inicializar no decritor.
+int inicializa_noDesc(noDesc *no){
+    if(no == NULL) return ERR; 
+
+    *no = malloc(sizeof*(*no));
+
+    if(*no == NULL) return ERR; 
+    
+    (*no)->ini = NULL;
+    (*no)->fim = NULL;
+
+    return OK;
+}
+
+// Função para enfileiramento em filas, utilizando no descritor
+int enfileirar(noDesc *desc, int elem){
+
+}
+
+// Função para desenfileiramento em filas, utilizando no descritor
+int desenfileirar(noDesc *desc, int *elem){
+    
+}

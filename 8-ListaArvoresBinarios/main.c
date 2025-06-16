a1cdae8ddab1bb061fa44ba1357f640cf6afd40e
Caio Macedo <caiomacedo123@outlook.com>
2025-06-15 21:17:31 -0300

    Os arquivo ai

---
+++ 8-ListaArvoresBinarios/main.c
@@ -0,0 +1,15 @@
+/*
+
+* 8 - Arvores Binárias | - Algoritmos e Estruturas de Dados III 
+* Autores:             | > Caio Macedo Lima da Cruz.....RA: 2651378
+*                      | > José Carlos Seben............RA: 2651130
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArvoreBin.h"
+
+int main() { 
+    
+}

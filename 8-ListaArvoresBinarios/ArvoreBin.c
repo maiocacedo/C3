ccaa7db9541c6456bedf61e11b4ef74f8dda6bd2
Caio Macedo <caiomacedo123@outlook.com>
2025-06-18 19:22:44 -0300

    Iniciando ArvoresAVL

--- 8-ListaArvoresBinarios/ArvoreBin.c
+++ 8-ListaArvoresBinarios/ArvoreBin.c
@@ -143,3 +143,71 @@ void preOrdem_ArvBin(ArvBin *raiz){
         preOrdem_ArvBin(&((*raiz)->dir));
     }
 }
+// Exercício 1 - Contar Folhas na arvoré
+/* A arvore é percorrida por recursão, indo para esquerda e direita, até que encontre as folhas, nós que 
+não possuem filhos. Casos bases são, quando o ponteiro da raiz for nulo ou a raiz for nula, retornando zero e 
+quando os ponteiros dos nós a direita e esquerda de raiz foram ambos nulos, retornando um. E o caso geral é a 
+soma do número de folhas na subárvore esquerda com o número de folhas na subárvore direita.
+*/
+int contaFolhas(ArvBin *raiz){
+    if(raiz == NULL || *raiz == NULL)
+        return 0;
+    if((*raiz)->esq == NULL && (*raiz)->dir == NULL)
+        return 1;    
+    return contaFolhas(&((*raiz)->esq)) + contaFolhas(&((*raiz)->dir));
+}
+
+// Exercício 2 - Imprimir Arvore em detalhes
+/* Para resolução desse exercício, foram utilizadas duas funções, uma auxiliar para imprimir cada nó,
+e uma para chamar a auxiliar. A função auxiliar opera de modo recursivo, onde ela recebe o no e o seu pai
+e a partir disso, ela imprimi o no, o pai, no esquerdo e o no direito. Depois ela é chamada de novo, só que 
+para o nó à esquerda e para o nó à direita
+*/
+void imprimeNo(struct NO* no, struct NO* pai) {
+    if (no == NULL) {
+        printf("NULL ");
+        return;
+    }
+    printf("No: %d, pai:  ", no->info);
+    if (pai != NULL) printf("%d", pai->info);
+    else printf("NULL");
+
+    printf(" Esquerdo:");
+    if (no->esq != NULL) printf("%d", no->esq->info);
+    else printf("NULL");
+
+    printf(" Direito:");
+    if (no->dir != NULL) printf("%d", no->dir->info);
+    else printf("NULL");
+
+    if (no->dir == NULL && no->esq == NULL) printf("[FOLHA]");
+
+    imprimeNo(no->esq, no);
+    imprimeNo(no->dir, no);
+}
+
+// Função de Imprimir arvore
+void imprimeArvore(ArvBin *raiz){
+    if (raiz == NULL || *raiz == NULL){
+        printf("Arvore vazia ou nula");
+        return;
+    }
+
+    imprimeNo(*raiz, NULL);
+}
+
+// Exercício 3 - Conta repetições
+/*
+
+*/
+int consultaNoRepetido(ArvBin *raiz, int valor){
+    if(raiz == NULL) return 0;
+    
+    int count = ((*raiz)->info == valor) ? 1 : 0;
+
+    count += consultaNoRepetido(&(*raiz)->esq, valor);
+    count += consultaNoRepetido(&(*raiz)->dir, valor);
+    return count;
+}
+
+// Exercício 4 - 

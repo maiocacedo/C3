2595a90297d99d9c364abe90fbb36a5573d3df3e
José C. Seben <josecarlospc001@gmail.com>
2025-06-27 11:07:36 -0300

    Iniciando lista - Grafos

---
+++ 10-ListaGrafos/main.c
@@ -0,0 +1,34 @@
+/*
+
+* 10 - Grafos - Algoritmos e Estruturas de Dados III 
+* Autores: | > Caio Macedo Lima da Cruz.....RA: 2651378
+*          | > José Carlos Seben............RA: 2651130
+
+*/
+
+#include "Grafo.h"
+
+int main(){
+    int eh_digrafo = 1;
+    Grafo* gr = cria_Grafo(5, 5, 0);
+
+    insereAresta(gr, 0, 1, eh_digrafo, 0);
+    insereAresta(gr, 1, 3, eh_digrafo, 0);
+    insereAresta(gr, 1, 2, eh_digrafo, 0);
+    insereAresta(gr, 2, 4, eh_digrafo, 0);
+    insereAresta(gr, 3, 0, eh_digrafo, 0);
+    insereAresta(gr, 3, 4, eh_digrafo, 0);
+    insereAresta(gr, 4, 1, eh_digrafo, 0);
+
+    imprime_Grafo(gr);
+    printf("\nBusca \n");
+
+    libera_Grafo(gr);
+
+    system("pause");
+
+    printf("\nAtividade 1 - Representacao vizual de grafo direcional e nao direcional\n");
+    printf("\nResposta da atividade 1 esta contida no PDF de respostas\n\n");
+    system("pause");
+    return 0;
+}
